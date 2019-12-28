7a5866b4224c6198d57dbe3c52df2890cd440664
yonureker <yonureker@gmail.com>
2019-12-27 16:11:20 -0800

    conenct frontend to backend

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [message, setMessage] = useState("")
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/api/users/test")
+    .then(response => response.json())
+    .then(responseJson => setMessage(responseJson.msg))
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
