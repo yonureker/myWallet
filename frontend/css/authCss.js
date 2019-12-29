c4a1b7826280441460a21d9ef8b5f0445e5e5580
yonureker <yonureker@gmail.com>
2019-12-29 14:52:56 -0800

    move css to external folder

---
+++ frontend/css/authCss.js
@@ -0,0 +1,44 @@
+export default authCss = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%"
+  },
+  textLink: {
+    color: "#49AEB5"
+  },
+  loginContainer: {
+    width: "100%",
+    minWidth: 400,
+    borderWidth: 1,
+    borderColor: "black"
+  },
+  textInput: {
+    width: "100%",
+    minWidth: "80%",
+    minHeight: 50,
+    borderWidth: 1.5,
+    borderColor: "#D7DBDD",
+    marginTop: 10,
+    padding: 10
+  },
+  socialLogin: {
+    marginTop: 10
+  },
+  button: {
+    marginTop: 10,
+    color: "#fff",
+    backgroundColor: "green",
+    minHeight: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  signupContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+    flexDirection: "row"
+  }
+}
--- frontend/screens/loginScreen.js
+++ frontend/screens/loginScreen.js
@@ -1,22 +1,55 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from "react-native";
+
+import AuthCss from '../css/authCss'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>
-        This is the Login Screen
-      </Text>
-    </View>
-  )
+      <View style={styles.loginModule}>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Email"
+            placeholderTextColor="#D7DBDD"
+            onChangeText={email => setEmail(email)}
+          />
+          <TextInput
+            style={styles.textInput}
+            secureTextEntry={true}
+            placeholder="Password"
+            placeholderTextColor="#D7DBDD"
+            onChangeText={password => setPassword(password)}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              loginUser(email, password);
+            }}
+          >
+            <Text style={{ fontSize: 20, color: "#ffffff" }}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+          <View style={styles.signupContainer}>
+          <Text>
+            Don't have an account?{" "}
+            <Text
+              style={styles.textLink}
+              onPress={() => props.navigation.navigate("Signup")}
+            >
+              Sign Up.
+            </Text>
+          </Text>
+          </View>
+        </View>
+      </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const styles = StyleSheet.create(AuthCss);
 
 export default LoginScreen;
