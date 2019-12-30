3974c91683b18e16c70a1b46179d99d4cddbe591
yonureker <yonureker@gmail.com>
2019-12-30 00:44:38 -0800

    add database connection

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const db = process.env.ATLAS_URI;
 
-app.get("/", (req, res) => res.send("hello"))
+app.get("/", (req, res) => res.send("hello"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
