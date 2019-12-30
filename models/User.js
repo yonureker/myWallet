2ccba92f99fba4000cb9e7443ca8ec35dd03f601
yonureker <yonureker@gmail.com>
2019-12-30 03:06:38 -0800

    update user schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema ({
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
