34d2213f229eb871984f3fe4739fd1b5bbff6f10
yonureker <yonureker@gmail.com>
2019-12-30 01:54:46 -0800

    add test route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-const User = require('../../models/User');
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
 
-router.post('/')
+  // Simple validation
+  // Return error if field is empty
+  if (!email || !password) {
+    return res.status(400).json({msg: 'Email or password can not be empty.'})
+  }
+
+  // Check if user with the same email exists
+  User.findOne({ email })
+    .then(user => {
+      if(user) return res.status(400).json({ msg: 'User already exists' });
+
+      const newUser = new User({
+        email,
+        password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+    })
+});
+
+
+
+module.exports = router;
