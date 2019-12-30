266e0572593f2ed93093ccdfe0414f71ec864d55
yonureker <yonureker@gmail.com>
2019-12-30 03:06:56 -0800

    styling

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,38 +1,63 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
+// User Model
+const User = require("../../models/User");
+
+// User Sign In
 router.post("/register", (req, res) => {
   const { email, password } = req.body;
 
   // Simple validation
   // Return error if field is empty
   if (!email || !password) {
-    return res.status(400).json({msg: 'Email or password can not be empty.'})
+    return res.status(400).json({ msg: "Email or password can not be empty." });
   }
 
   // Check if user with the same email exists
-  User.findOne({ email })
-    .then(user => {
-      if(user) return res.status(400).json({ msg: 'User already exists' });
+  User.findOne({ email }).then(user => {
+    if (user) return res.status(400).json({ msg: "User already exists" });
 
-      const newUser = new User({
-        email,
-        password
+    const newUser = new User({
+      email,
+      password
+    });
+
+    console.log(newUser);
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        console.log(newUser.password);
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
       });
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser.save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        })
-      })
-    })
+    });
+  });
 });
 
+// User Log In
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
 
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ email: "This user does not exist" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Incorrect password" });
+      }
+    });
+  });
+});
 
 module.exports = router;
