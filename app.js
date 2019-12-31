0b0991fdab8cd5ce494a61a98d6d1ca78dd5f5e6
yonureker <yonureker@gmail.com>
2019-12-30 23:24:41 -0800

    add passport

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-require("dotenv").config();
 const express = require("express");
-const app = express();
+const passport = require('passport');
 const mongoose = require("mongoose");
+
+// DB and Port configs
 const db = process.env.ATLAS_URI;
 const port = process.env.PORT || 5000;
 
+// Routes
 const users = require("./routes/api/users");
 const items = require("./routes/api/items");
 
-app.get("/", (req, res) => res.send("hello"));
+const app = express();
+
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api/users", users);
 app.use("/api/items", items);
+app.use(passport.initialize());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+// Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
+
+// To be able to use .env file
+require("dotenv").config();
+
+
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const secret = process.env.SECRET;
+const secret = process.env.secretOrKeys;
 
 // User Model
 const User = require("../../models/User");
