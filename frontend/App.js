742e1eebde9001624e2cfc7680c0d877c2f0825c
yonureker <yonureker@gmail.com>
2020-01-01 15:39:46 -0800

    remove comments

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
 
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import LoginScreen from './screens/loginScreen'
-import SignupScreen from './screens/signupScreen'
-import ItemsScreen from './screens/itemsScreen'
-// import ProfileScreen from './screens/profileScreen'
-// import PasswordScreen from './screens/passwordScreen'
+import LoginScreen from "./screens/loginScreen";
+import SignupScreen from "./screens/signupScreen";
+import ItemsScreen from "./screens/itemsScreen";
 
 export default function App() {
+
   return (
       <AppContainer />
   );
@@ -21,11 +19,9 @@ const AppNavigator = createStackNavigator(
     Login: LoginScreen,
     Signup: SignupScreen,
     Items: ItemsScreen
-    // Profile: ProfileScreen,
-    // Password: PasswordScreen
   },
   {
-    initialRouteName: 'Items'
+    initialRouteName: "Login"
   }
 );
 
