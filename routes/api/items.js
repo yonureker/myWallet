a5fdd69f5c4e0707c5cab2256f103473b3870ac3
yonureker <yonureker@gmail.com>
2020-01-01 15:39:23 -0800

    update item validation logic

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,7 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.post("/add", (req, res) => res.json({ msg: "This is the users route" }));
-router.get("/items", (req, res) => res.json({ msg: "This is the users route" }));
+const Item = require('../../models/Item');
+const validateItemInput = require('../../validation/items');
+
+// Gets a list of items for a specific user
+router.get('/user/:user_id', (req, res) => {
+  Item.find({user: req.params.user_id})
+  .then(items => res.json(items))
+  .catch(err => res.status(404).json({ noitemsfound: 'No Items found'}))
+})
+
+router.post('/', 
+  passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    const {errors, isValid} = validateItemInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newItem = new Item({
+      user: req.user.id,
+      name: req.body.name,
+      amount: req.body.amount,
+      description: req.body.description
+    });
+
+    newItem.save().then(item => res.json(item));
+  }
+);
 
 module.exports = router;
+
+
---
+++ validation/items.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateTweetInput(data) {
+  let errors = {};
+
+  data.name = validText(data.text) ? data.text : '';
+  data.amount = validText(data.amount) ? data.amount : '';
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
+  if (Validator.isEmpty(data.amount)) {
+    errors.amount = 'Amount field is required';
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
