8672e0a9d1e2be30182fe8bb155bd99403ce13e8
yonureker <yonureker@gmail.com>
2020-01-02 17:02:21 -0800

    update ItemScreen

--- frontend/screens/itemsScreen.js
+++ frontend/screens/itemsScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, FlatList } from "react-native";
+import ItemCard from '../components/itemCard';
 
 const ItemsScreen = props => {
+  const [items, setItems] = useState([]);
   // logged in User
   const userId = props.navigation.state.params.userId
 
@@ -15,29 +17,37 @@ const ItemsScreen = props => {
     });
 
     const data = await response.json()
-
-    //data
-  //   [
-  //     {
-  //         "_id": "5e0db1e33a7890be1b47ecac",
-  //         "user": "5e0c58c827c9988d06fe9d86",
-  //         "name": "Netflix",
-  //         "amount": 50,
-  //         "description": "hello",
-  //         "date": "2020-01-02T09:03:31.067Z",
-  //         "__v": 0
-  //     }
-  // ]
-    console.log()
+    setItems(data);
   }
 
-  
+  const addNewItem = () => {
+    return(
+      <TouchableOpacity>
+        <Text>Add New Item</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  useEffect(() => {
+    receiveItems(userId);
+  });
 
   return (
    
     <View style={styles.container}>
-      {receiveItems(userId)}
-      {console.log(receiveItems())}
+       <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ItemCard
+            id={item._id}
+            title={item.name}
+            amount={item.amount}
+            description={item.description}
+          />
+        )}
+        keyExtractor={item => item._id}
+        ListFooterComponent={addNewItem}
+      />
     </View>
   );
 };
@@ -46,7 +56,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   }
 });
 
