1dd853f35565dafe9e28d6d9d60a691115d06886
yonureker <yonureker@gmail.com>
2020-01-03 13:41:37 -0800

    create addItems screen

---
+++ frontend/screens/addItemsScreen.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  Alert
+} from "react-native";
+
+import AuthCss from '../css/authCss';
+
+const AddItemsScreen = props => {
+  const [name, setName] = useState("")
+  const [amount, setAmount] = useState(0)
+  const [description, setDescription] = useState("")
+
+   // logged in User
+   const { userId, token } = props.navigation.state.params;
+
+  const addItem = async (userId, name, amount, description) => {
+    try {const response = await fetch("http://localhost:5000/api/items/new/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: token
+      },
+      body: JSON.stringify({
+        user: userId,
+        name: name,
+        amount: amount,
+        description: description
+      })
+    })
+
+    const data = await response.json();
+
+    if (data.id) {
+      props.navigation.navigate('Items');
+    } else {
+      const errors = Object.values(data);
+
+      errors.map(error => Alert.alert(error))
+    }
+  } catch {
+    console.log(error)
+  }}
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.loginModule}>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            autoCapitalize="none"
+            placeholder="Name"
+            placeholderTextColor="#D7DBDD"
+            onChangeText={name => setName(name)}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Amount"
+            placeholderTextColor="#D7DBDD"
+            keyboardType="numeric"
+            onChangeText={amount => setAmount(amount)}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Description"
+            placeholderTextColor="#D7DBDD"
+            onChangeText={description => setDescription(description)}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              addItem(userId, name, amount, description);
+            }}
+          >
+            <Text style={{ fontSize: 20, color: "#ffffff" }}>Add Item</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
+}
+
+AddItemsScreen.navigationOptions = {
+  title: "Add Item"
+};
+
+const styles = StyleSheet.create(AuthCss);
+
+export default AddItemsScreen;
