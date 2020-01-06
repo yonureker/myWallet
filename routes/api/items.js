64d35986ed3a38dced8df1740c365a9d79ca1039
yonureker <yonureker@gmail.com>
2020-01-06 02:27:39 -0800

    add update route

--- routes/api/items.js
+++ routes/api/items.js
@@ -37,18 +37,37 @@ router.post(
 );
 
 // Delete an item
-router.delete("/:item_id",
-  (req, res) => {
-    Item.findByIdAndDelete(req.body._id, (error, data) => {
+router.delete("/:item_id", (req, res) => {
+  Item.findByIdAndDelete(req.body._id, (error, data) => {
+    if (error) {
+      console.log("could not delete item something is wrong");
+      return res.status(400).json({ message: "Item is not deleted" });
+    } else {
+      console.log("deleted");
+      return res.json({ message: `item deleted` });
+    }
+  });
+});
+
+//
+router.put("/:item_id", (req, res) => {
+  Item.findByIdAndUpdate(
+    req.body._id,
+    {
+      name: req.body.name,
+      amount: req.body.amount,
+      description: req.body.description
+    },
+    (error, data) => {
       if (error) {
-        console.log('could not delete item something is wrong');
-        return res.status(400).json({message: 'Item is not deleted'})
+        console.log("could not edit item something is wrong");
+        return res.status(400).json({ message: "Item is not edited" });
       } else {
-        console.log('deleted');
-        return res.json({message: `item deleted`});
+        console.log("edited");
+        return res.json({ message: `item edited` });
       }
-    })});
-
-
+    }
+  );
+});
 
 module.exports = router;
