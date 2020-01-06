885dd428dc6367d4b961a32fe4e84da6cfe56411
yonureker <yonureker@gmail.com>
2020-01-05 23:21:31 -0800

    add edit screen

--- frontend/App.js
+++ frontend/App.js
@@ -7,9 +7,9 @@ import LoginScreen from "./screens/loginScreen";
 import SignupScreen from "./screens/signupScreen";
 import ItemsScreen from "./screens/itemsScreen";
 import AddItemsScreen from "./screens/addItemsScreen";
+import EditItemsScreen from './screens/editItemsScreen'
 
 export default function App() {
-
   return (
       <AppContainer />
   );
@@ -20,7 +20,8 @@ const AppNavigator = createStackNavigator(
     Login: LoginScreen,
     Signup: SignupScreen,
     Items: ItemsScreen,
-    AddItems: AddItemsScreen
+    AddItems: AddItemsScreen,
+    EditItems: EditItemsScreen
   },
   {
     initialRouteName: "Login"
